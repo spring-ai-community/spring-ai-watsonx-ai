d44cc24eb3a4b9c1bd4a968ff23aa08f08f99e6d
Tristan Mahinay <rjt.mahinay@gmail.com>
2026-02-16 02:59:06 +0800

    docs(pre-1.1.0): remove IBM logo and simplify homepage header
    Signed-off-by: Tristan Mahinay <rjt.mahinay@gmail.com>

--- docs-site/src/pages/index.js
+++ docs-site/src/pages/index.js
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
@@ -13,11 +12,6 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img
-          src={useBaseUrl('/img/ibm-logo.svg')}
-          alt="IBM Logo"
-          className={styles.heroLogo}
-        />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -30,7 +24,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs">
-            Getting Started - 5min ⏱️
+            Get Started
           </Link>
           <Link
             className="button button--outline button--secondary button--lg"
